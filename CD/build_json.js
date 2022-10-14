4eb208f38e7560d6f872f0236a646d28ec9e2996
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2022-10-14 22:08:28 +0900

    CD: convert yaml on build

---
+++ CD/build_json.js
@@ -0,0 +1,41 @@
+import chalk from "chalk";
+import fs from "fs";
+import jsYaml from "js-yaml";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+function walk(dir, callback) {
+  fs.readdir(dir, (err, files) => {
+    if (err) {
+      console.log(err);
+    } else {
+      files.forEach((file) => {
+        const pathname = path.join(dir, file);
+        fs.stat(pathname, (err, stats) => {
+          if (err) {
+            console.log(err);
+          } else if (stats.isDirectory()) {
+            walk(pathname, callback);
+          } else {
+            callback(pathname);
+          }
+        });
+      });
+    }
+  });
+}
+
+walk(path.resolve(__dirname, "..", "public", "config", "yaml"), (pathname) => {
+  try {
+    const yaml = fs.readFileSync(pathname, "utf8");
+    const json = jsYaml.load(yaml);
+    const jsonPath = pathname.replaceAll("yaml", "json");
+    fs.writeFileSync(jsonPath, JSON.stringify(json, null, 0));
+    console.log(`[${chalk.green("SUCCESS")}] ${jsonPath}`);
+  } catch (e) {
+    console.log(`[${chalk.red("ERROR")}] ${pathname}\n${chalk.red(e)}`);
+  }
+});
