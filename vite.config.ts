0f280cf5282aa2f4ae73201502a33734ca42eab1
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2022-11-24 16:47:56 +0900

    fix: Vue defineProps 不能 import 的问题

--- vite.config.ts
+++ vite.config.ts
@@ -1,55 +1,57 @@
-import { visualizer } from "rollup-plugin-visualizer";
-import { defineConfig } from "vite";
-import viteCompression from "vite-plugin-compression";
-import { VitePWA } from "vite-plugin-pwa";
-import legacy from "@vitejs/plugin-legacy";
-import vue from "@vitejs/plugin-vue";
+import { visualizer } from 'rollup-plugin-visualizer';
+import { defineConfig } from 'vite';
+import viteCompression from 'vite-plugin-compression';
+import { VitePWA } from 'vite-plugin-pwa';
+import VueTypeImports from 'vite-plugin-vue-type-imports';
+import legacy from '@vitejs/plugin-legacy';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VueTypeImports(),
     legacy({
       targets: [
-        "Android >= 39",
-        "Chrome >= 50",
-        "Safari >= 10.1",
-        "iOS >= 10.3",
-        "> 1%",
+        'Android >= 39',
+        'Chrome >= 50',
+        'Safari >= 10.1',
+        'iOS >= 10.3',
+        '> 1%',
       ],
     }),
     VitePWA({
-      injectRegister: "auto",
-      registerType: "autoUpdate",
+      injectRegister: 'auto',
+      registerType: 'autoUpdate',
       includeManifestIcons: true,
-      includeAssets: ["favicon/*.png"],
+      includeAssets: ['favicon/*.png'],
       manifest: {
-        name: "Blue Archive Story Viewer",
-        short_name: "BA Stories",
-        description: "Yet another Blue Archive story viewer",
-        lang: "zh-CN",
-        theme_color: "#62abe9",
+        name: 'Blue Archive Story Viewer',
+        short_name: 'BA Stories',
+        description: 'Yet another Blue Archive story viewer',
+        lang: 'zh-CN',
+        theme_color: '#62abe9',
         icons: [
           {
-            src: "/favicon/apple-touch-icon.png",
-            sizes: "180x180",
-            type: "image/png",
+            src: '/favicon/apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png',
           },
           {
-            src: "/favicon/maskable-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "maskable",
+            src: '/favicon/maskable-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable',
           },
           {
-            src: "/favicon/android-chrome-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
+            src: '/favicon/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
           },
           {
-            src: "/favicon/android-chrome-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
+            src: '/favicon/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
           },
         ],
       },
@@ -57,23 +59,23 @@ export default defineConfig({
         runtimeCaching: [
           {
             urlPattern: /(.*?)\.(mp4|mp3|wav)$/i,
-            handler: "CacheFirst",
+            handler: 'CacheFirst',
             options: {
-              cacheName: "multimedia-cache",
+              cacheName: 'multimedia-cache',
             },
           },
           {
             urlPattern: /(.*?)\.(png|jpe?g|svg|gif|webp|ico)$/i,
-            handler: "StaleWhileRevalidate",
+            handler: 'StaleWhileRevalidate',
             options: {
-              cacheName: "image-cache",
+              cacheName: 'image-cache',
             },
           },
           {
             urlPattern: /(.*?)\.(js|css|json)$/i,
-            handler: "NetworkFirst",
+            handler: 'NetworkFirst',
             options: {
-              cacheName: "asset-cache",
+              cacheName: 'asset-cache',
               networkTimeoutSeconds: 5,
             },
           },
@@ -87,7 +89,7 @@ export default defineConfig({
   ],
   build: {
     emptyOutDir: true,
-    minify: "terser",
+    minify: 'terser',
     terserOptions: {
       toplevel: true,
       safari10: true,
