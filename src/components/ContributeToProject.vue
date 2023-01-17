d3aaabab9680cc960f3ab8cf52a2bd8aba06b0ea
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2023-01-18 00:25:11 +0900

    chore: 更新贡献者页面，close #108

--- src/components/ContributeToProject.vue
+++ src/components/ContributeToProject.vue
@@ -1,3 +1,40 @@
+<script setup lang="ts">
+const contributors = [
+  {
+    name: 'Shinku_cc',
+    role: '前端',
+  },
+  {
+    name: 'ourandream',
+    role: '前端',
+  },
+  {
+    name: '海星',
+    role: '前端',
+  },
+  {
+    name: 'Z_DK',
+    role: '前端',
+  },
+  {
+    name: 'Notype',
+    role: '自动化',
+  },
+  {
+    name: '第一个mt',
+    role: '全栈',
+  },
+  {
+    name: '骑猪追月',
+    role: '后端',
+  },
+  {
+    name: 'EllisNewman',
+    role: '解包，美术',
+  },
+];
+</script>
+
 <template>
   <div id="contributor-container" class="flex-vertical">
     <h2>成为贡献者</h2>
@@ -8,134 +45,114 @@
       src="/src/assets/roadmap.webp"
       alt="路线图"
     />
-    <br />
     <p>我们需要：</p>
     <ul>
       <li>
-        <mark>项目经理</mark>：有项目管理经验（团队有 20
-        个人），对前后端技术和架构有一定了解；
+        <mark>前端</mark>：基于 Vue3 + pinia + TypeScript +
+        <a href="https://github.com/pixijs/pixijs" target="_blank">PixiJS</a>
+        开发
+        <a href="https://github.com/ba-archive/ba-story-player" target="_blank"
+          >剧情播放器</a
+        >
+        和后续的剧情编辑器；
+      </li>
+      <li><mark>架构</mark>：设计前后端数据交换规范；</li>
+      <li>
+        <mark>后端</mark>：基于 Go 或 Java 开发检索后端；（目前倾向于 Go）
       </li>
       <li>
-        <mark>前端</mark>：熟悉 Vue3 + pinia + TypeScript 的开发模式, 愿意加入
+        <mark>服务器运维</mark>：维护我们的小破 Ubuntu
+        服务器，合理配置服务器系统防火墙和 openresty 服务；
+      </li>
+      <li>
+        <mark>自动化（Ops）</mark>：帮助团队建立从
+        <a href="https://github.com/ba-archive/weaverbird" target="_blank"
+          >资源更新</a
+        >
+        到部署的自动化 CI/CD 管线；
+      </li>
+      <li><mark>美术</mark>：整体视觉设计和 UI 交互；</li>
+      <li><mark>LOGO</mark>：比现在的好看就行；以及</li>
+      <li><mark>正在查看内容的你</mark>：汇报 bug，提出意见或建议</li>
+      <li>
         <a
-          href="https://github.com/ba-archive/blue-archive-story-viewer"
+          href="https://github.com/ba-archive/blue-archive-story-viewer/discussions/97"
           target="_blank"
-          >项目建设</a
-        >；
+          >更多信息请点此查看详情</a
+        >
       </li>
-      <li><mark>后端</mark>：有基于 Go / Java 的后端开发经验；</li>
-      <li>
-        <mark>服务器运维</mark>：有 Linux
-        服务器运维经验，会合理配置服务器系统防火墙和 openresty 服务；
-      </li>
-      <li>
-        <mark>DevOps</mark>：能够帮助团队建立从资源更新到部署的自动化 CI/CD
-        管线；
-      </li>
-      <li><mark>架构</mark>：有前后端架构设计经验（想试试手也可以）；</li>
-      <li><mark>UI</mark>：会使用 Figma 等工具提供原型图等图像资产；</li>
-      <li><mark>正在查看内容的你</mark>：汇报 bug，意见或建议</li>
-
-      <p>
-        欢迎联系！
-        <a
-          href="https://message.bilibili.com/#/whisper/mid14066311"
-          target="_blank"
-          >（电脑用户点这里）</a
-        >，
-        <a href="https://space.bilibili.com/14066311" target="_blank"
-          >（手机用户点这里）</a
-        >，
-        <a href="mailto:mail@blue-archive.io" target="_blank"
-          >（邮箱地址点这里）</a
-        >
-      </p>
-
-      <h2>同时感谢以下朋友的贡献：（排名不分先后）</h2>
-      <p>
-        <a href="https://nga.178.com/nuke.php?func=ucp&uid=60668025"
-          >Shinku_cc</a
-        >
-        前端
-      </p>
-      <p>
-        <a href="https://nga.178.com/nuke.php?func=ucp&uid=64394869"
-          >ourandream</a
-        >
-        前端
-      </p>
-      <p>_Aisle 前端</p>
-      <p>
-        <a href="https://nga.178.com/nuke.php?func=ucp&uid=60919291"
-          >EllisNewman</a
-        >
-        解包，美术
-      </p>
-      <p>
-        <a href="https://nga.178.com/nuke.php?func=ucp&uid=63222501">陆合正</a>
-        momotalk 资源提供
-      </p>
-      <p>
-        <a href="https://nga.178.com/nuke.php?func=ucp&uid=1761181">骑猪追月</a>
-        后端
-      </p>
-      <p>
-        <a href="https://nga.178.com/nuke.php?func=ucp&uid=42164110"
-          >第一个mt</a
-        >
-        全栈
-      </p>
-      <p>以及每一个热爱碧蓝档案的玩家</p>
     </ul>
+    <p>
+      欢迎联系！
+      <a
+        href="https://message.bilibili.com/#/whisper/mid14066311"
+        target="_blank"
+        >（B站私信点这里）</a
+      >，
+      <a href="mailto:mail@blue-archive.io" target="_blank"
+        >（邮件联系点这里）</a
+      >
+    </p>
+
+    <h2>同时感谢以下贡献者：（排名不分先后）</h2>
+    <!-- eslint-disable vue/require-v-for-key -->
+    <p v-for="contributor in contributors" v-once>
+      {{ contributor.name }}：{{ contributor.role }}
+    </p>
+    <!--eslint-enable vue/require-v-for-key-->
+    <p>以及每一个热爱碧蓝档案的玩家</p>
   </div>
 </template>
 
-<script setup lang="ts">
-//
-</script>
-
 <style scoped lang="scss">
 #contributor-container {
-  align-items: center;
+  align-items: flex-start;
   align-self: center;
+  transition: color 0.375s ease-in-out;
+  padding-bottom: 2rem;
   width: min(50rem, 90%);
-}
-
-h2 {
-  align-self: center;
-  margin-top: 2rem;
-  margin-bottom: 0.5rem;
-}
-
-p,
-li {
-  margin-top: 1rem;
-}
-
-li {
-  list-style: none;
-
-  &::before {
-    margin-right: 0.5rem;
-    content: '•';
-  }
-}
-
-a {
   color: var(--color-text-main);
-}
 
-.roadmap {
-  max-width: min(50rem, 90%);
-}
+  h2 {
+    align-self: center;
+    margin-top: 2rem;
+    margin-bottom: 0.5rem;
+  }
 
-mark {
-  background-color: transparent;
-  font-weight: bold;
-  text-decoration-line: underline;
-  text-decoration-color: cornflowerblue;
-  text-decoration-thickness: 0.2rem;
-  text-underline-offset: 0.2rem;
+  p,
+  li {
+    margin-top: 1rem;
+  }
+
+  li {
+    list-style: none;
+
+    &::before {
+      margin-right: 0.5rem;
+      content: '•';
+    }
+  }
+
+  a {
+    transition: color 0.375s ease-in-out;
+    color: var(--color-text-main);
+  }
+
+  .roadmap {
+    margin: 1rem 0;
+    max-width: min(50rem, 90%);
+  }
+
+  mark {
+    transition: color 0.375s ease-in-out;
+    background-color: transparent;
+    color: var(--color-text-main);
+    font-weight: bold;
+    text-decoration-line: underline;
+    text-decoration-color: cornflowerblue;
+    text-decoration-thickness: 0.2rem;
+    text-underline-offset: 0.2rem;
+  }
 }
 
 @media screen and (max-width: 768px) {
